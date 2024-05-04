49d5e68593a101b08cab44b53ec0fdaf4e5ac1c7
lin <550210817@qq.com>
2024-05-04 20:32:17 +0800

    feat:添加输入框

--- src/views/Home/Chat/components/ChatBox/SendBar/index.vue
+++ src/views/Home/Chat/components/ChatBox/SendBar/index.vue
@@ -1,58 +1,58 @@
 <script setup lang="ts" name="SendBar">
 import { computed, onBeforeUnmount, onMounted, provide, reactive, ref } from 'vue'
-// import type { ElInput } from 'element-plus'
-// import { ElMessage } from 'element-plus'
+import type { ElInput } from 'element-plus'
+import { ElMessage } from 'element-plus'
 import { useWsLoginStore } from '@/stores/ws'
 import { useUserStore } from '@/stores/user'
 import { useChatStore } from '@/stores/chat'
 import { useGlobalStore } from '@/stores/global'
 // import { useGroupStore } from '@/stores/group'
 // import { useUserInfo } from '@/hooks/useCached'
-// import MsgInput from '../MsgInput/index.vue'
-// import { insertInputText } from '../MsgInput/utils'
-// import apis from '@/services/apis'
+import MsgInput from '../MsgInput/index.vue'
+import { insertInputText } from '../MsgInput/utils'
+import apis from '@/services/apis'
 import { judgeClient } from '@/utils/detectDevice'
-// import { emojis } from '../constant'
-// import { useEmojiStore } from '@/stores/emoji'
-// import { MsgEnum, RoleEnum } from '@/enums'
+import { emojis } from '../constant'
+import { useEmojiStore } from '@/stores/emoji'
+import { MsgEnum, RoleEnum } from '@/enums'
 
-// import type { IMention } from '../MsgInput/types'
+import type { IMention } from '../MsgInput/types'
 // import { useFileDialog } from '@vueuse/core'
-// import { useUpload } from '@/hooks/useUpload'
-// import { useEmojiUpload } from '@/hooks/useEmojiUpload'
-// import { useRecording } from '@/hooks/useRecording'
+import { useUpload } from '@/hooks/useUpload'
+import { useEmojiUpload } from '@/hooks/useEmojiUpload'
+import { useRecording } from '@/hooks/useRecording'
 // import { useMockMessage } from '@/hooks/useMockMessage'
 // import { generateBody } from '@/utils'
 // import renderReplyContent from '@/utils/renderReplyContent'
 // import eventBus from '@/utils/eventBus'
-// import throttle from 'lodash/throttle'
+import throttle from 'lodash/throttle'
 
 const client = judgeClient()
 
 const chatStore = useChatStore()
 const globalStore = useGlobalStore()
-// const isSending = ref(false)
-// const inputMsg = ref('')
-// const mentionRef = ref<typeof ElInput>()
-// const mentionList = ref<IMention[]>([])
-// const isAudio = ref(false)
-// const isHovered = ref(false)
-// const tempMessageId = ref(0)
-// const showEmoji = ref(false)
-// const nowMsgType = ref<MsgEnum>(MsgEnum.FILE)
-// const panelIndex = ref(0)
-// const isUpEmoji = ref(false)
-// const tempEmojiId = ref(-1)
+const isSending = ref(false)
+const inputMsg = ref('')
+const mentionRef = ref<typeof ElInput>()
+const mentionList = ref<IMention[]>([])
+const isAudio = ref(false)
+const isHovered = ref(false)
+const tempMessageId = ref(0)
+const showEmoji = ref(false)
+const nowMsgType = ref<MsgEnum>(MsgEnum.FILE)
+const panelIndex = ref(0)
+const isUpEmoji = ref(false)
+const tempEmojiId = ref(-1)
 
-// const focusMsgInput = () => {
-//   setTimeout(() => {
-//     if (!mentionRef.value) return
-//     mentionRef.value?.focus?.()
-//     const selection = mentionRef.value?.range?.selection as Selection
-//     selection?.selectAllChildren(mentionRef.value.input)
-//     selection?.collapseToEnd()
-//   })
-// }
+const focusMsgInput = () => {
+  setTimeout(() => {
+    if (!mentionRef.value) return
+    mentionRef.value?.focus?.()
+    const selection = mentionRef.value?.range?.selection as Selection
+    selection?.selectAllChildren(mentionRef.value.input)
+    selection?.collapseToEnd()
+  })
+}
 // // 艾特
 // const onSelectPerson = ({ uid, ignoreCheck }: { uid: number; ignoreCheck?: boolean }) => {
 //   mentionRef.value?.onSelectPerson?.(uid, ignoreCheck)
@@ -68,7 +68,7 @@ const globalStore = useGlobalStore()
 //   eventBus.off('focusMsgInput', focusMsgInput)
 // })
 
-// // 发送消息
+// 发送消息
 // const send = (msgType: MsgEnum, body: any) => {
 //   apis
 //     .sendMsg({ roomId: globalStore.currentSession.roomId, msgType, body })
@@ -94,29 +94,29 @@ const globalStore = useGlobalStore()
 //     })
 // }
 
-// const sendMsgHandler = () => {
-//   // 空消息或正在发送时禁止发送
-//   if (!inputMsg.value?.trim().length || isSending.value) {
-//     return
-//   }
+const sendMsgHandler = () => {
+  // 空消息或正在发送时禁止发送
+  if (!inputMsg.value?.trim().length || isSending.value) {
+    return
+  }
 
-//   isSending.value = true
-//   send(MsgEnum.TEXT, {
-//     content: inputMsg.value,
-//     replyMsgId: currentMsgReply.value.message?.id,
-//     atUidList: mentionList.value.map((item) => item.uid),
-//   })
-// }
+  isSending.value = true
+  // send(MsgEnum.TEXT, {
+  //   content: inputMsg.value,
+  //   replyMsgId: currentMsgReply.value.message?.id,
+  //   atUidList: mentionList.value.map((item) => item.uid),
+  // })
+}
 
 const loginStore = useWsLoginStore() // 显示登录框
 const userStore = useUserStore() // 是否已登录
-// const emojiStore = useEmojiStore()
+const emojiStore = useEmojiStore()
 // const groupStore = useGroupStore()
 const isSign = computed(() => userStore.isSign)
 // const currentMsgReply = computed(() => (userStore.isSign && chatStore.currentMsgReply) || {})
 // const currentReplUid = computed(() => currentMsgReply?.value.fromUser?.uid as number)
 // const currentReplyUser = useUserInfo(currentReplUid)
-// const emojiList = computed(() => emojiStore.emojiList)
+const emojiList = computed(() => emojiStore.emojiList)
 
 // // 是否被提出群聊
 // const isRemoved = computed(() => groupStore.countInfo.role === RoleEnum.REMOVED)
@@ -130,46 +130,46 @@ const isSign = computed(() => userStore.isSign)
 
 // // 置空回复的消息
 // const onClearReply = () => (chatStore.currentMsgReply = {})
-// // 插入表情
-// const insertEmoji = (emoji: string) => {
-//   const input = mentionRef.value?.input
-//   const editRange = mentionRef.value?.range as {
-//     range: Range
-//     selection: Selection
-//   }
-//   if (!input || !editRange) return
-//   insertInputText({ content: emoji, ...editRange })
-//   // 需要更新以触发 onChang
-//   inputMsg.value = input.innerText
-//   // 关闭表情弹窗，一次只选一个表情
-//   showEmoji.value = false
-//   // 临时让获取焦点
-//   focusMsgInput()
-// }
+// 插入表情
+const insertEmoji = (emoji: string) => {
+  const input = mentionRef.value?.input
+  const editRange = mentionRef.value?.range as {
+    range: Range
+    selection: Selection
+  }
+  if (!input || !editRange) return
+  insertInputText({ content: emoji, ...editRange })
+  // 需要更新以触发 onChang
+  inputMsg.value = input.innerText
+  // 关闭表情弹窗，一次只选一个表情
+  showEmoji.value = false
+  // 临时让获取焦点
+  focusMsgInput()
+}
 
-// const onInputChange = (val: string, mentions: IMention[]) => {
-//   mentionList.value = mentions
-// }
-// const options = reactive({ multiple: false, accept: '.jpg,.png' })
+const onInputChange = (val: string, mentions: IMention[]) => {
+  mentionList.value = mentions
+}
+const options = reactive({ multiple: false, accept: '.jpg,.png' })
 
 // const { open, reset, onChange } = useFileDialog(options)
-// const { isUploading, fileInfo, uploadFile, onStart, onChange: useUploadChange } = useUpload()
-// const { uploadEmoji, isEmojiUp } = useEmojiUpload()
-// const { isRecording, start, stop, onEnd, second } = useRecording()
+const { isUploading, fileInfo, uploadFile, onStart, onChange: useUploadChange } = useUpload()
+const { uploadEmoji, isEmojiUp } = useEmojiUpload()
+const { isRecording, start, stop, onEnd, second } = useRecording()
 // const { mockMessage } = useMockMessage()
 
-// const openFileSelect = (fileType: string, isEmoji = false) => {
-//   if (fileType === 'img') {
-//     nowMsgType.value = MsgEnum.IMAGE
-//     options.accept = '.jpg,.png,.gif,.jpeg,.webp'
-//   }
-//   if (fileType === 'file') {
-//     nowMsgType.value = MsgEnum.FILE
-//     options.accept = '*' // 任意文件
-//   }
-//   isUpEmoji.value = isEmoji
-//   open()
-// }
+const openFileSelect = (fileType: string, isEmoji = false) => {
+  if (fileType === 'img') {
+    nowMsgType.value = MsgEnum.IMAGE
+    options.accept = '.jpg,.png,.gif,.jpeg,.webp'
+  }
+  if (fileType === 'file') {
+    nowMsgType.value = MsgEnum.FILE
+    options.accept = '*' // 任意文件
+  }
+  isUpEmoji.value = isEmoji
+  open()
+}
 
 // const selectAndUploadFile = async (files?: FileList | null) => {
 //   if (!files?.length) return
@@ -221,20 +221,20 @@ const isSign = computed(() => userStore.isSign)
 
 // onEnd((audioFile: any) => uploadFile(audioFile))
 
-// const onStartRecord = () => {
-//   nowMsgType.value = MsgEnum.VOICE
-//   start()
-// }
+const onStartRecord = () => {
+  nowMsgType.value = MsgEnum.VOICE
+  start()
+}
 
-// const handleRightClick = (event: Event, id: number) => {
-//   event.preventDefault()
-//   tempEmojiId.value = tempEmojiId.value === id ? -1 : id
-// }
+const handleRightClick = (event: Event, id: number) => {
+  event.preventDefault()
+  tempEmojiId.value = tempEmojiId.value === id ? -1 : id
+}
 
-// const sendEmoji = throttle((url: string) => {
-//   send(MsgEnum.EMOJI, { url })
-//   showEmoji.value = false
-// }, 1000)
+const sendEmoji = throttle((url: string) => {
+  // send(MsgEnum.EMOJI, { url })
+  showEmoji.value = false
+}, 1000)
 </script>
 
 <template>
@@ -246,7 +246,7 @@ const isSign = computed(() => userStore.isSign)
       </ElIcon>
     </div> -->
     <div class="msg-input">
-      <!-- <div class="action" @click="isAudio = !isAudio">
+      <div class="action" @click="isAudio = !isAudio">
         <Icon v-show="!isAudio" icon="voice" class="audio" />
         <Icon v-show="isAudio" icon="jianpan" />
       </div>
@@ -285,8 +285,8 @@ const isSign = computed(() => userStore.isSign)
       >
         <template #reference>
           <div class="action" @mouseover="isHovered = true" @mouseleave="isHovered = false">
-            <Icon v-if="isHovered" icon="shocked" :size="18" colorful />
-            <Icon v-else icon="happy1" :size="18" colorful />
+            <Icon v-if="isHovered" icon="face" :size="19" colorful />
+            <Icon v-else icon="face" :size="19" colorful />
           </div>
         </template>
         <div class="emoji-panel">
@@ -348,28 +348,28 @@ const isSign = computed(() => userStore.isSign)
       <Icon
         class="action"
         icon="at"
-        :size="20"
+        :size="22"
         colorful
         @click="insertInputText({ content: '@', ...mentionRef?.range })"
       />
       <Icon
         :class="['action', { disabled: isUploading }]"
-        icon="tupian"
-        :size="18"
+        icon="img"
+        :size="16"
         colorful
         @click="openFileSelect('img')"
       />
-      <Icon class="action" icon="wenjianjia2" :size="20" colorful @click="openFileSelect('file')" />
+      <Icon class="action" icon="file" :size="22" colorful @click="openFileSelect('file')" />
       <div class="divider" />
       <div
         :class="['action', { 'is-edit': inputMsg.length, 'disabled': !inputMsg.length }]"
         @click="sendMsgHandler"
       >
-        <Icon class="send" icon="huojian" :size="20" />
-      </div> -->
+        <Icon class="send" icon="send" :size="20" colorful />
+      </div>
     </div>
     <span v-if="!isSign" class="tips" @click="loginStore.showLogin = true">
-      <ElIcon class="icon-lock"><IEpLock /></ElIcon>
+      <ElIcon class="icon-lock" ><IEpLock /></ElIcon>
       点我<span class="tips-text">登录</span>之后再发言~
     </span>
     <!-- <span v-if="isSign && isRemoved" class="tips"> 您已被踢出群聊，无法再发送消息 </span> -->
