1d0224ce361b484a9cc8fe321d7e816591ec399b
lin <550210817@qq.com>
2024-05-17 16:32:19 +0800

    feat:外部访问

--- config/vite.config.base.ts
+++ config/vite.config.base.ts
@@ -32,6 +32,10 @@ export default defineConfig({
     }),
     Icons({ autoInstall: true }),
   ],
+  server: {
+    //使用IP能访问
+      host: '0.0.0.0'
+  },
   resolve: {
     alias: {
       '@':'/src/'
